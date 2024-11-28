30ab80f4ad9e63565f5c7be05151c5d918a88844
KauanMO <kauaanmatheus@gmail.com>
2024-11-28 17:20:38 -0300

    feat: Estrutura inicial

---
+++ src/test/java/com/duckmail/DuckmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
