34ea1c1052283d529f0bb639be8baca728b75887
KauanMO <kauaanmatheus@gmail.com>
2024-12-12 11:09:09 -0300

    style: Envio de email programado em consumer do RabbitMQ

--- src/main/java/com/duckmail/infra/rabbitmq/RabbitEmailConsumer.java
+++ src/main/java/com/duckmail/infra/rabbitmq/RabbitEmailConsumer.java
@@ -1,6 +1,7 @@
 package com.duckmail.infra.rabbitmq;
 
 import com.duckmail.dtos.email.QueuedEmailDTO;
+import com.duckmail.infra.email.EmailSenderService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitEmailConsumer {
     private final ObjectMapper objectMapper;
+    private final EmailSenderService emailSenderService;
 
-    public RabbitEmailConsumer(ObjectMapper objectMapper) {
+    public RabbitEmailConsumer(ObjectMapper objectMapper, EmailSenderService emailSenderService) {
         this.objectMapper = objectMapper;
+        this.emailSenderService = emailSenderService;
     }
 
     public void consumeMessage(String message) {
         try {
             var queuedEmail = objectMapper.readValue(message, QueuedEmailDTO.class);
+
+            emailSenderService.sendQueuedEmail(queuedEmail);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
