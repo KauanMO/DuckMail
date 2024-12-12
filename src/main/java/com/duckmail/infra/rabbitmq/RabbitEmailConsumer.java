09585b807c8d64f8bb1f3da5e6d2c21de06f6a89
KauanMO <kauaanmatheus@gmail.com>
2024-12-12 11:17:40 -0300

    style: Cadastro de delivery error log

--- src/main/java/com/duckmail/infra/rabbitmq/RabbitEmailConsumer.java
+++ src/main/java/com/duckmail/infra/rabbitmq/RabbitEmailConsumer.java
@@ -2,18 +2,24 @@ package com.duckmail.infra.rabbitmq;
 
 import com.duckmail.dtos.email.QueuedEmailDTO;
 import com.duckmail.infra.email.EmailSenderService;
+import com.duckmail.services.DeliveryErrorLogService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Component
 public class RabbitEmailConsumer {
     private final ObjectMapper objectMapper;
     private final EmailSenderService emailSenderService;
+    private final DeliveryErrorLogService deliveryErrorLogService;
 
-    public RabbitEmailConsumer(ObjectMapper objectMapper, EmailSenderService emailSenderService) {
+    public RabbitEmailConsumer(ObjectMapper objectMapper, EmailSenderService emailSenderService, DeliveryErrorLogService deliveryErrorLogService) {
         this.objectMapper = objectMapper;
         this.emailSenderService = emailSenderService;
+        this.deliveryErrorLogService = deliveryErrorLogService;
     }
 
     public void consumeMessage(String message) {
@@ -22,7 +28,7 @@ public class RabbitEmailConsumer {
 
             emailSenderService.sendQueuedEmail(queuedEmail);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            deliveryErrorLogService.registerError(e.getMessage(), LocalDateTime.now().atZone(ZoneId.of("America/Sao_Paulo")).toLocalDateTime());
         }
     }
 }
