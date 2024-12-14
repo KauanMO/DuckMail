82031235bc6bbe542f9ac9567adb1d6ddf072e93
KauanMO <kauaanmatheus@gmail.com>
2024-12-14 12:31:04 -0300

    style: Busca de total de aberturas de email para dashboard

--- src/main/java/com/duckmail/services/impl/DashboardServiceImpl.java
+++ src/main/java/com/duckmail/services/impl/DashboardServiceImpl.java
@@ -2,6 +2,7 @@ package com.duckmail.services.impl;
 
 import com.duckmail.repositories.ClickHistoryRepository;
 import com.duckmail.repositories.DeliveryErrorLogRepository;
+import com.duckmail.repositories.OpenHistoryRepository;
 import com.duckmail.services.DashboardService;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 public class DashboardServiceImpl implements DashboardService {
     private final DeliveryErrorLogRepository deliveryErrorLogRepository;
     private final ClickHistoryRepository clickHistoryRepository;
+    private final OpenHistoryRepository openHistoryRepository;
 
-    public DashboardServiceImpl(DeliveryErrorLogRepository deliveryErrorLogRepository, ClickHistoryRepository clickHistoryRepository) {
+    public DashboardServiceImpl(DeliveryErrorLogRepository deliveryErrorLogRepository, ClickHistoryRepository clickHistoryRepository, OpenHistoryRepository openHistoryRepository) {
         this.deliveryErrorLogRepository = deliveryErrorLogRepository;
         this.clickHistoryRepository = clickHistoryRepository;
+        this.openHistoryRepository = openHistoryRepository;
     }
 
     @Override
@@ -23,5 +26,10 @@ public class DashboardServiceImpl implements DashboardService {
     @Override
     public Long getTotalClickHistories() {
         return clickHistoryRepository.count();
+    }
+
+    @Override
+    public Long getTotalOpenHistories() {
+        return openHistoryRepository.count();
     }
 }
