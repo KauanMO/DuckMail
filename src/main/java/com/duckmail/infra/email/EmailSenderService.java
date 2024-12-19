f96dc012839a3ab61977f604767117a231281a61
KauanMO <kauaanmatheus@gmail.com>
2024-12-19 10:29:14 -0300

    feat: Chamada de requisição de registro de Open History e corpo de email

--- src/main/java/com/duckmail/infra/email/EmailSenderService.java
+++ src/main/java/com/duckmail/infra/email/EmailSenderService.java
@@ -3,11 +3,15 @@ package com.duckmail.infra.email;
 import com.duckmail.dtos.email.QueuedEmailDTO;
 import com.duckmail.services.DeliveryErrorLogService;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -25,22 +29,27 @@ public class EmailSenderService {
     }
 
     public void sendQueuedEmail(QueuedEmailDTO emailDTO) {
-        sendEmail(emailDTO.to(), emailDTO.subject(), emailDTO.body());
+        sendEmail(emailDTO.to(), emailDTO.subject(), emailDTO.body(), emailDTO.recipientId());
     }
 
     @Async
-    protected void sendEmail(String to, String subject, String body) {
+    protected void sendEmail(String to, String subject, String body, Long recipientId) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
 
             helper.setFrom(EMAIL_FROM);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(body, true);
+
+            Path path = Paths.get("src/main/java/com/duckmail/assets/logo.png");
+            Resource resource = new FileSystemResource(path.toFile());
+            helper.addInline("logo", resource);
+
             javaMailSender.send(mimeMessage);
         } catch (Exception e) {
-            deliveryErrorLogService.registerError(e.getMessage(), LocalDateTime.now().atZone(ZoneId.of("America/Sao_Paulo")).toLocalDateTime());
+            deliveryErrorLogService.registerError(e.getMessage(), LocalDateTime.now().atZone(ZoneId.of("America/Sao_Paulo")).toLocalDateTime(), recipientId);
         }
     }
 }
