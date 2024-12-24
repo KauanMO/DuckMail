d1b6f4f90a90e11560a9226705a3ce441e0ab9e9
KauanMO <kauaanmatheus@gmail.com>
2024-12-24 15:00:01 -0300

    feat: Mudança de status de destinatário ao envio de email

--- src/main/java/com/duckmail/services/RecipientService.java
+++ src/main/java/com/duckmail/services/RecipientService.java
@@ -2,10 +2,11 @@ package com.duckmail.services;
 
 import com.duckmail.dtos.recipient.InRecipientDTO;
 import com.duckmail.dtos.recipient.OutValidRecipientsSegregationDTO;
+import com.duckmail.enums.RecipientStatus;
 import com.duckmail.models.Recipient;
 
 public interface RecipientService extends Writable<Recipient, InRecipientDTO>,
         Readable<Recipient, Long>,
         ListWritable<OutValidRecipientsSegregationDTO, InRecipientDTO> {
-
+    Recipient changeRecipientStatus(Long id, RecipientStatus newStatus);
 }
