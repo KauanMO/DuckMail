c6132e61197a275cbaa0a39c769e0747e0fae30f
KauanMO <kauaanmatheus@gmail.com>
2025-01-18 18:21:43 -0300

    feat: Atualizando status de destinatário ao envio ou falha de envio de email por Lambda

--- src/main/java/com/duckmail/services/LambdaService.java
+++ src/main/java/com/duckmail/services/LambdaService.java
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 
 public interface LambdaService<T> {
-    ResponseEntity<String> invokeLambda(T dto) throws JsonProcessingException;
+    void invokeLambda(T dto) throws JsonProcessingException;
 }
--- src/main/java/com/duckmail/services/impl/EmailSenderLambdaService.java
+++ src/main/java/com/duckmail/services/impl/EmailSenderLambdaService.java
@@ -4,28 +4,36 @@ import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
 import com.duckmail.dtos.email.QueuedEmailDTO;
+import com.duckmail.enums.RecipientStatus;
+import com.duckmail.services.DeliveryErrorLogService;
 import com.duckmail.services.LambdaService;
+import com.duckmail.services.RecipientService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Service
 public class EmailSenderLambdaService implements LambdaService<QueuedEmailDTO> {
     private final AWSLambda awsLambda;
+    private final DeliveryErrorLogService deliveryErrorLogService;
+    private final RecipientService recipientService;
 
     @Value("${aws.lambda.mail-sender-function}")
     private String emailSenderFunction;
 
-    public EmailSenderLambdaService(AWSLambda awsLambda) {
+    public EmailSenderLambdaService(AWSLambda awsLambda, DeliveryErrorLogService deliveryErrorLogService, RecipientService recipientService) {
         this.awsLambda = awsLambda;
+        this.deliveryErrorLogService = deliveryErrorLogService;
+        this.recipientService = recipientService;
     }
 
     @Override
-    public ResponseEntity<String> invokeLambda(QueuedEmailDTO dto) throws JsonProcessingException {
+    public void invokeLambda(QueuedEmailDTO dto) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         String dtoJson = mapper.writeValueAsString(dto);
 
@@ -36,6 +44,13 @@ public class EmailSenderLambdaService implements LambdaService<QueuedEmailDTO> {
 
         InvokeResult result = awsLambda.invoke(request);
 
-        return ResponseEntity.status(result.getStatusCode()).body(StandardCharsets.UTF_8.decode(result.getPayload()).toString());
+        if (result.getStatusCode().equals(HttpStatus.OK.value())) {
+            recipientService.changeRecipientStatus(dto.recipientId(), RecipientStatus.SENT);
+        } else {
+            recipientService.changeRecipientStatus(dto.recipientId(), RecipientStatus.FAILED);
+            deliveryErrorLogService.registerError(StandardCharsets.UTF_8.decode(result.getPayload()).toString(),
+                    LocalDateTime.now(),
+                    dto.recipientId());
+        }
     }
 }
